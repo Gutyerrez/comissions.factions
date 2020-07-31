73b98e6d9bb33810323a021447d76ff95ae74ead
Gutyerrez <srgutyerrez@gmail.com>
2020-07-31 07:15:26 -0300

    dev

--- battle/src/main/java/io/github/gutyerrez/factions/battle/api/battle/Battle.java
+++ battle/src/main/java/io/github/gutyerrez/factions/battle/api/battle/Battle.java
@@ -1,22 +1,40 @@
 package io.github.gutyerrez.factions.battle.api.battle;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.Date;
 
 /**
  * @author SrGutyerrez
  */
+@ToString
 @AllArgsConstructor
+@EqualsAndHashCode(of = { "factionId", "targetId", "status", "createdAt" })
 public class Battle {
 
     @Getter
+    @NonNull
     private final String factionId;
+
+    @Getter
+    @NonNull
     private final String targetId;
+
+    @Getter
+    private Status status;
+
+    @Getter
+    @NonNull
     private final Date createdAt;
 
+    @Getter
     private Date updatedAt;
+
+    public void start() {
+        this.status = Status.STARTED;
+
+
+    }
 
     public enum Status {
 
